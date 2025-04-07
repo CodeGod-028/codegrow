de4c9da2f70a54f68bb9d4e3e30a9a193ef6c3fb
Kratos768 <nimcet2k24@gmail.com>
2025-04-07 12:03:16 +0530

    users

--- users.php
+++ users.php
@@ -4,6 +4,7 @@ include("db.php");
 include("header.php");
 
 if(isset($_POST['username'])){
+$user_id = $_POST['user_id'];
 $username = $_POST['username'];
 $email = $_POST['email'];
 $phone = $_POST['phone'];
@@ -12,7 +13,16 @@ $role = $_POST['role'];
 
 date_default_timezone_set("Asia/Kolkata"); // Set timezone to India
 $currentDate = date("Y-m-d H:i:s"); // Format: 2025-04-05 14:23:45
+if(isset($_POST['update'])){
+  $query = mysqli_query($conn,"update users set
+   username = '$username',
+   email = '$email',
+   phone = '$$phone',
+   password = '$password',
+   role = '$role' where user_id = '$user_id'
+   ");
 
+}else{
 $query = mysqli_query($conn, "insert into users set 
 username = '$username', 
 email = '$email', 
@@ -21,10 +31,16 @@ password = '$password',
 role = '$role', 
 created_at = '$currentDate'
 ");
+}
 
 if($query)
 {
-  echo "<script> alert('User added Succesfully!'); </script>"; 
+  if(isset($_POST['update'])){
+    echo "<script> alert('User Updated Succesfully!'); </script>"; 
+  }else{
+    echo "<script> alert('User Added Succesfully!'); </script>"; 
+  }
+  
 }
 else{
   echo "<script> alert('Query Failed'); </script>"; 
@@ -38,7 +54,7 @@ else{
 <h2>Add New Participant</h2>
 
 <form method="POST" action="">
-  <input type="hidden" id="user_id"> 
+  <input type="hidden" id="user_id" name="user_id"> 
   <label for="username">Username</label>
   <input type="text" id="username" name="username" required>
 
@@ -46,7 +62,7 @@ else{
   <input type="email" id="email" name="email" required>
 
   <label for="phone">Phone</label>
-  <input type="tel" id="phone" name="phone" required>
+  <input type="number" id="phone" name="phone" required>
 
   <label for="password">Password</label>
   <input type="password" id="password" name="password" required>
@@ -57,7 +73,8 @@ else{
     <option value="setter">Setter</option>
   </select>
 
-  <button type="submit">Add Participant</button>
+  <button id="add_btn" type="submit"  name="insert">Add Participant</button>
+  <button id="update_btn" style="display:none;"type="submit" name="update">Update Participant</button>
 </form>
 
 <hr style="margin: 40px 0;">
@@ -97,7 +114,14 @@ if(mysqli_num_rows($result)>0){
 <td><?=$data['password'];?></td>
 <td><?=$data['role'];?></td>
 <td><?=$data['created_at'];?></td>
-<td><button>Edit</button>
+<td><button id="btn_edit" class="btn_edit" 
+data-id="<?= $data['user_id'] ?>"
+data-username="<?= $data['username'] ?>"
+data-email="<?= $data['email'] ?>"
+data-phone="<?= $data['phone'] ?>"
+data-password="<?= $data['password'] ?>"
+data-role="<?= $data['role'] ?>"
+>Edit</button>
 <button>Delete</button></td>
 </tr>
 <?php 
@@ -107,9 +131,33 @@ if(mysqli_num_rows($result)>0){
   </tbody>
 </table>  
 
-
-
 </main>
 </div>
+<script>
+  $(document).ready(function(){
+
+  $(".btn_edit").click(function(){
+    var id = $(this).data('id');
+    var username = $(this).data('username');
+    var email = $(this).data('email');
+    var phone = $(this).data('phone')
+    var password = $(this).data('password');
+    var role = $(this).data('role');
+
+    $('#user_id').val(id);
+    $('#username').val(username);
+    $('#email').val(email);
+    $('#phone').val(phone)
+    $('#role').val(role);
+    $('#password').val(password);
+
+    $('#add_btn').css("display","none");
+    $('#update_btn').css("display","block");
+
+    window.scrollTo(0, 0); // scroll to top
+    
+  });
+  });
+</script>
 </body>
 </html>
